768d1085b88f16fac38cb8109e2533bff6ec9a40
YezQiu <sakayezq@gmail.com>
2025-08-19 13:37:33 +0800

    feat: Initial commit
    Initialize project structure for pyclog, a custom logging library.

--- pyclog/__init__.py
+++ pyclog/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.1.0"
 
 from .writer import ClogWriter
 from .reader import ClogReader
-from .handler import ClogFileHandler
+from .handler import ClogFileHandler, ClogRotatingFileHandler
 from .exceptions import (
     ClogError,
     InvalidClogFileError,
@@ -17,6 +17,7 @@ __all__ = [
     "ClogWriter",
     "ClogReader",
     "ClogFileHandler",
+    "ClogRotatingFileHandler", 
     "ClogError",
     "InvalidClogFileError",
     "UnsupportedCompressionError",
